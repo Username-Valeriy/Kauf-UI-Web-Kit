9ebc3c80abd5dcf225752a437be839a6665992ed
Valera <krivoy212010@gmail.com>
2018-02-27 16:51:29 +0200

    fool

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 
 
   $('.video-slider').slick({
+    prevArrow: '<span class="vidoe-arow lnr lnr-chevron-left" ></span>',
+    nextArrow: '<span class="vidoe-arow lnr lnr-chevron-right"></span>',
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 1,
@@ -19,6 +21,8 @@ $(document).ready(function(){
 
   });
   $('.product-slider').slick({
+    prevArrow: '<span class=" lnr lnr-chevron-left" ></span>',
+    nextArrow: '<span class=" lnr lnr-chevron-right"></span>',
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
